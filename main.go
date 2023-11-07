100be06aeb28bab9456546e979635bd563bcc6e8
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-11-07 13:40:14 +0200

    Add method for getting required solver configuration

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
@@ -15,6 +21,18 @@ import (
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
+
+// ErrNoAllowedZonesConfigured is returned when the solver was not
+// configured with a list of allowed zones.
+var ErrNoAllowedZonesConfigured = errors.New("no allowed zones configured")
+
+// ErrNoTSIGKeyConfigured is returned when the solver was not
+// configured with a TSIG key.
+var ErrNoTSIGKeyConfigured = errors.New("no TSIG key configured")
+
+// DefaultTTL represents the default TTL value to set for new records,
+// unless specified in the configuration
+const DefaultTTL = 300
 
 func main() {
 	if GroupName == "" {
@@ -50,10 +68,6 @@ type bindProviderConfig struct {
 	// update the DNS records.
 	TSIGKeyRef cmmeta.SecretKeySelector `json:"tsigKeyRef"`
 
-	// ZoneName is the DNS zone name we are managing for
-	// _acme-challenge TXT records.
-	ZoneName string `json:"zoneName"`
-
 	// TTL is the time-to-live to set on the newly created TXT
 	// records
 	TTL int `json:"ttl"`
@@ -61,6 +75,10 @@ type bindProviderConfig struct {
 	// AllowedZones is the list of zones that the solver is
 	// allowed to manage
 	AllowedZones []string `json:"allowedZones"`
+
+	// tsigKey represents the raw TSIG key after fetching it from
+	// the secret store
+	tsigKey string
 }
 
 // Name implements the webhook.Solver interface
@@ -71,10 +89,12 @@ func (b *bindProviderSolver) Name() string {
 // Present implements the webhook.Solver interface by creating the
 // respective TXT records
 func (b *bindProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
-	cfg, err := loadConfig(ch.Config)
+	cfg, err := b.loadConfig(ch.Config, ch.ResourceNamespace)
 	if err != nil {
 		return err
 	}
+
+	klog.InfoS("Solving challenge", "dnsName", ch.DNSName, "resolvedZone", ch.ResolvedZone, "resolvedFQDN", ch.ResolvedFQDN)
 
 	// TODO: do something more useful with the decoded configuration
 	fmt.Printf("Decoded configuration %v", cfg)
@@ -104,15 +124,49 @@ func (b *bindProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-
 
 // loadConfig is a small helper function that decodes JSON configuration into
 // the typed config struct.
-func loadConfig(cfgJSON *extapi.JSON) (bindProviderConfig, error) {
-	cfg := bindProviderConfig{}
+func (b *bindProviderSolver) loadConfig(cfgJSON *extapi.JSON, namespace string) (bindProviderConfig, error) {
+	cfg := bindProviderConfig{
+		TTL: DefaultTTL,
+	}
+
+	// We require TSIG key and allowed zones to be configured
 	if cfgJSON == nil {
-		return cfg, nil
+		return cfg, errors.New("TSIG key and allowedZones must be configured")
 	}
 
 	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
 		return cfg, fmt.Errorf("error decoding solver config: %v", err)
 	}
+
+	// Validate the configuration and set sane defaults, if
+	// needed.
+	if cfg.TTL == 0 {
+		cfg.TTL = DefaultTTL
+	}
+
+	if cfg.AllowedZones == nil {
+		return cfg, ErrNoAllowedZonesConfigured
+	}
+
+	if cfg.TSIGKeyRef.LocalObjectReference.Name == "" {
+		return cfg, ErrNoTSIGKeyConfigured
+	}
+
+	// Load the TSIG key
+	ctx := context.Background()
+	getOpts := metav1.GetOptions{}
+	tsigSecret, err := b.client.CoreV1().Secrets(namespace).Get(ctx, cfg.TSIGKeyRef.LocalObjectReference.Name, getOpts)
+
+	if err != nil {
+		return cfg, fmt.Errorf("failed to load TSIG key from %s/%s: %v", namespace, cfg.TSIGKeyRef.LocalObjectReference.Name, err)
+	}
+
+	secretData, ok := tsigSecret.Data[cfg.TSIGKeyRef.Key]
+	if !ok {
+		return cfg, fmt.Errorf("TSIG key %s not found in %s/%s", cfg.TSIGKeyRef.Key, cfg.TSIGKeyRef.LocalObjectReference.Name, namespace)
+	}
+
+	cfg.tsigKey = string(secretData)
 
 	return cfg, nil
 }
